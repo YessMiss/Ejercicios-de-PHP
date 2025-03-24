daa82fec3bb5a17d167919d12ffc382caefcb833
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:57:39 -0600

    Create alumnos.php

---
+++ alumnos.php
@@ -0,0 +1,14 @@
+<!-- formulario.php -->
+<html>
+<head>
+    <title>Consultar Alumno</title>
+</head>
+<body>
+    <h1>Consultar Alumno</h1>
+    <form action="consultar_alumno.php" method="POST">
+        Ingrese el nombre del alumno: <input type="text" name="nombre" required>
+        <br><br>
+        <input type="submit" value="Buscar Alumno">
+    </form>
+</body>
+</html>
