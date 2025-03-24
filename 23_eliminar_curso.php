a550531bdde68ebae3089e3709004fc66bd1d8bf
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:59:40 -0600

    Create 23_eliminar_curso.php

---
+++ 23_eliminar_curso.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Eliminar Curso</title>
+</head>
+<body>
+    <h2>Eliminar un Curso</h2>
+    <form action="23_eliminar_curso_proceso.php" method="POST">
+        Ingrese el nombre del curso que desea eliminar:
+        <input type="text" name="nombrecurso" required>
+        <br><br>
+        <input type="submit" value="Eliminar Curso">
+    </form>
+</body>
+</html>
