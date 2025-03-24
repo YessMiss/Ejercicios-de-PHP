6a7b9b30e974826136b6c32bfb24fc05b3988adf
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:33:09 -0600

    Create 22_pag1.php

---
+++ 22_pag1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Ejemplo Pagina 1 Libreria</title>
+</head>
+<body>
+<?php include("21_libreria.phtml"); ?>
+<?php CabeceraPagina(); ?>
+
+Página 1
+<br><br><br><br><br>
+
+Contenido blalbl blalb alb<br><br> más cosas...<br><br>
+fin<br><br>
+
+<?php PiePagina(); ?>
+
+</body>
+</html>
