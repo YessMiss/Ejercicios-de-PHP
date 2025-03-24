6ba015510753aed724a096e5c2b1cb40f4d437a4
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:27:52 -0600

    Create practica8.php

---
+++ practica8.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Ejemplo de Sustitución de Variables en String</title>
+</head>
+<body>
+<h1>Incorporación de Variables en un String</h1>
+
+<?php
+// Definimos tres variables enteras
+$precio = 90;
+$cantidad = 2;
+$total = $precio * $cantidad;
+
+// Definimos un string que incorpora las variables
+echo "El precio de un artículo es $precio, la cantidad es $cantidad, y el total es $total.";
+?>
+
+</body>
+</html>
