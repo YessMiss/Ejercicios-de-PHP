709b128dfe092304d564f4a7a795ae783a7bbbdf
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:22:49 -0600

    Create weekdays2.php

---
+++ weekdays2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Introducción a los arrays, método corto</title>
+</head>
+<body>
+    <h1>Introducción a los arrays, método corto</h1>
+    <p>A continuación escribiremos los arrays de acuerdo al método corto</p>
+    <p>
+    <?php
+    /*
+    Este es el método corto para la creación de arrays, donde todo el array puede estar en una sola línea de código.
+    */
+    $dia = array("domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado");
+
+    // Mostrar el miércoles
+    echo $dia[3];  // Salida: miércoles
+    ?>
+    </p>
+</body>
+</html>
