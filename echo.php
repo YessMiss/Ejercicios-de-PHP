6f7f1ebd5772147a8d90b446f8a383e81d86a230
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:19:39 -0600

    Create echo.php

---
+++ echo.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+  <title>Mensajes con Echo</title>
+</head>
+
+<body>
+  <h1>Mensajes con el comando Echo</h1>
+
+  <?php
+    // Mostramos el primer mensaje
+    echo "¡Hola! Bienvenido a mi página web.<br>";
+    
+    // Mostramos el segundo mensaje
+    echo "Este es un mensaje usando el comando echo.<br>";
+    
+    // Mostramos el tercer mensaje
+    echo "PHP es un lenguaje de programación poderoso.<br>";
+    
+    // Mostramos el cuarto mensaje
+    echo "¡Gracias por visitar mi página!<br>";
+  ?>
+
+</body>
+
+</html>
