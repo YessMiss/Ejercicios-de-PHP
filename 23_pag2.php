f6196b393f0df88fe3f8a1e13fcaf56cf5aacece
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:33:42 -0600

    Create 23_pag2.php

---
+++ 23_pag2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Ejemplo Pagina 2 Librerias</title>
+</head>
+
+<body>
+<?php include("21_libreria.phtml"); ?>
+<?php CabeceraPagina(); ?>
+Esta es otra página<br><br> completamente distinta<br><br>
+pero comparte el pie y la cabecera con la otra.<br><br>
+
+<?php PiePagina(); ?>
+</body>
+</html>
