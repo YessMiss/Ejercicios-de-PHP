e744c65cf0cb8e3d0260a337be4ef991465f8dde
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:20:44 -0600

    Create variable2.php

---
+++ variable2.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+  <title>Segundo ejemplo de Variables</title>
+</head>
+
+<body>
+  <h1>Segundo ejemplo de Variables usando el operador de concatenación (.)</h1>
+  <p>
+    <?php
+      $Name = "Miguel";
+      echo "Hola <b>" . $Name . "</b>, encantado de conocerte";
+    ?>
+  </p>
+</body>
+
+</html>
