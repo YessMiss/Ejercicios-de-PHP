144066b83496a7ae7c009d0f078f1e1d03115421
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:22:13 -0600

    Create weekdays1.php

---
+++ weekdays1.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Introducción a los arrays, método largo</title>
+</head>
+<body>
+    <h1> Introducción a los arrays, método largo </h1>
+    <p> A continuación, escribiremos los arrays de acuerdo al método largo </p>
+    <p>
+    <?php
+    /*
+    Este es el método largo para la creación de arrays, donde cada uno de los arreglos 
+    está en una línea diferente, junto con su valor índice correspondiente.
+    */
+    $dia[0] = "domingo";
+    $dia[1] = "lunes";
+    $dia[2] = "martes";
+    $dia[3] = "miércoles";
+    $dia[4] = "jueves";
+    $dia[5] = "viernes";
+    $dia[6] = "sábado";
+
+    // Mostrar el miércoles
+    echo $dia[3];
+    ?>
+    </p>
+</body>
+</html>
