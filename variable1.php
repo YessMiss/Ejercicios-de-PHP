271c74d0576b9b578228b622bc706ba4dacbb327
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:20:14 -0600

    Create variable1.php

---
+++ variable1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+  <title>Primer ejemplo de Variables</title>
+</head>
+
+<body>
+  <h1>Primer ejemplo de Variables</h1>
+  <p>
+    <?php
+      $Name = "Miguel";
+      echo "Hola <b>$Name</b>, encantado de conocerte";
+    ?>
+  </p>
+</body>
+
+</html>
