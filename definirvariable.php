24c685d4aecfbd87e7225b0421da3e09ae130bb9
YessMiss <146153799+YessMiss@users.noreply.github.com>
2025-03-23 19:27:26 -0600

    Create definirvariable.php

---
+++ definirvariable.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Definición y Visualización de Variables</title>
+</head>
+<body>
+<h1>Variables de Diferentes Tipos en PHP</h1>
+
+<?php
+// Definición de variables
+$integerVar = 42;           // Tipo Integer
+$doubleVar = 3.14;          // Tipo Double
+$stringVar = "Hola, Mundo"; // Tipo String
+$booleanVar = true;         // Tipo Boolean
+
+// Impresión de las variables
+echo "El valor de la variable integer es: $integerVar <br>";
+echo "El valor de la variable double es: $doubleVar <br>";
+echo "El valor de la variable string es: $stringVar <br>";
+echo "El valor de la variable boolean es: $booleanVar <br>";
+?>
+
+</body>
+</html>
